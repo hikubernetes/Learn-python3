6a2ecf923adc3032652078006cdc60642d38ec5f
chanrbo <chanrbo@gmail.com>
2019-02-01 16:27:47 +0800

    socket

--- socket/echo-client.py
+++ socket/echo-client.py
@@ -1,11 +1,11 @@
 import socket
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-#建立一个连接
+# 建立连接:
 s.connect(('127.0.0.1', 9999))
-#接收消息
+# 接收欢迎消息:
 print(s.recv(1024).decode('utf-8'))
-for data in [b'bob', b'echo', b'mike']:
-    #发送数据
+for data in [b'Michael', b'Tracy', b'Sarah']:
+    # 发送数据:
     s.send(data)
     print(s.recv(1024).decode('utf-8'))
 s.send(b'exit')
